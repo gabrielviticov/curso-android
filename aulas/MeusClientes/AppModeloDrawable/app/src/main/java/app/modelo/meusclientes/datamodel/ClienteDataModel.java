1c2a8f6a02f25bc103a54e8b8a4f5e8a686adac6
Gabriel Viticov Plata de Souza <gabrielviticov.ctt@gmail.com>
2022-09-25 10:09:40 -0300

    as atividades do dia foram upadas

---
+++ aulas/MeusClientes/AppModeloDrawable/app/src/main/java/app/modelo/meusclientes/datamodel/ClienteDataModel.java
@@ -0,0 +1,56 @@
+package app.modelo.meusclientes.datamodel;
+
+public class ClienteDataModel {
+
+    // Modelo Objeto Relacional
+
+    // Toda Classe Data Model tem esta estrutura
+
+    // 5 - Queries de consulta gerais
+
+    // 1 - Atributo nome da tabela
+    public static final String TABELA = "cliente";
+
+    // 2 - Atributos um para um com os nomes dos campos
+
+    public static final String ID = "id"; // integer
+    public static final String NOME = "nome"; // text
+    public static final String TELEFONE = "telefone";
+    public static final String EMAIL = "email"; // text
+    public static final String CEP = "cep";
+    public static final String LOGRADOURO = "logradouro";
+    public static final String NUMERO = "numero";
+    public static final String BAIRRO = "bairro";
+    public static final String CIDADE = "cidade";
+    public static final String ESTADO = "estado";
+    public static final String TERMOS_DE_USO = "termos_de_uso"; //integer - 1 true, 0 - false
+
+    // 3 - Query para criar a tabela no banco de dados
+    public static String queryCriarTabela = "";
+
+    // // 4 - Método para gerar o Script para criar a tabela;
+
+    public static String criarTabela(){
+
+        // Concatenação de String
+
+        queryCriarTabela += "CREATE TABLE "+TABELA+" (";
+        queryCriarTabela += ID+" integer primary key autoincrement, ";
+        queryCriarTabela += NOME+" text, "; // nome text
+        queryCriarTabela += TELEFONE+" text, ";
+        queryCriarTabela += EMAIL+" text, ";
+        queryCriarTabela += CEP+" integer, ";
+        queryCriarTabela += LOGRADOURO+" text, ";
+        queryCriarTabela += NUMERO+" integer, ";
+        queryCriarTabela += BAIRRO+" text, ";
+        queryCriarTabela += CIDADE+" text, ";
+        queryCriarTabela += ESTADO+" text, ";
+        queryCriarTabela += TERMOS_DE_USO+" integer ";
+        queryCriarTabela += ")";
+
+        // queryCriarTabela = "Parte 01 Parte 02 Parte 03 Parte 04"
+
+        return queryCriarTabela;
+    }
+
+}
---
+++ aulas/MeusClientes/AppModeloDrawable/app/src/main/java/app/modelo/meusclientes/model/Cliente.java
@@ -0,0 +1,105 @@
+package app.modelo.meusclientes.model;
+
+// Pojo
+public class Cliente {
+
+    private Integer id;
+    private String nomeCompleto;
+    private String telefone;
+    private String email;
+    private Integer cep;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private Boolean termosDeUso;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Boolean getTermosDeUso() {
+        return termosDeUso;
+    }
+
+    public void setTermosDeUso(Boolean termosDeUso) {
+        this.termosDeUso = termosDeUso;
+    }
+}
